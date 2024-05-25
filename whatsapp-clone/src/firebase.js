b55d56ffc1a95d7d5e1f8ce22e1c1890f0c1fc93
Rithik2211 <rithikpranao22@gmail.com>
2024-05-25 21:48:30 +0530

    initial fire base setup

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0PYvjMuhGK4aIpkO1jQUrgpNMuHAI1bA",
+    authDomain: "mern-whatsapp-6a1aa.firebaseapp.com",
+    projectId: "mern-whatsapp-6a1aa",
+    storageBucket: "mern-whatsapp-6a1aa.appspot.com",
+    messagingSenderId: "659686663586",
+    appId: "1:659686663586:web:f3ac2b4c39857a506a7efb"
+  };
